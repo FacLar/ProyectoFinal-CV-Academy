02de18876f7ba003502543cdb3d853bc0e8e6199
F.Larrosa <facularrosa95@gmail.com>
2023-03-16 18:24:27 -0300

    archivo js

--- JS/app.js
+++ JS/app.js
@@ -8,12 +8,3 @@
   }
   document.getElementsByClassName("nav-bar").addEventListener("click",mostrarOcultarMenu)
 
-  //OCULTO MENU UNA VEZ QUE SELECIONO LINK// NO SUPE COMO RESOLVERLO, AYUDA!!!!!!!
-//function ocultarUlAlClick(){
-//   if(document.getElementsByTagName("nav ul li a").style.display == "block"){
-//        document.getElementsByTagName("nav ul li a").style.display = "none";
-//   }
-//document.getElementsByTagName("nav ul li a").addEventListener("click",ocultarUlAlClick)// 
-
-//tuve la intención de hacer un archivo pdf para mi cv descargable pero no entendí muy bien y tampoco tuve tiempo de emaparme bien
-//en html2pdf (canvas) y jspdf
